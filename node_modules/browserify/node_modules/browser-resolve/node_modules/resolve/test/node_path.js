51ae80f3a1ccec0d262efd0a5edae999661fcd20
Ian Groff <ian@iangroff.com>
2015-05-14 08:34:18 -0500

    updates driven by the pooling add

--- node_modules/browserify/node_modules/browser-resolve/node_modules/resolve/test/node_path.js
+++ node_modules/browserify/node_modules/browser-resolve/node_modules/resolve/test/node_path.js
@@ -1,8 +1,9 @@
-var test = require('tap').test;
+var path = require('path');
+var test = require('tape');
 var resolve = require('../');
 
 test('$NODE_PATH', function (t) {
-    t.plan(3);
+    t.plan(4);
     
     resolve('aaa', {
         paths: [
@@ -32,5 +33,16 @@ test('$NODE_PATH', function (t) {
         basedir: __dirname,
     }, function (err, res) {
         t.equal(res, __dirname + '/node_path/x/ccc/index.js');
+    });
+
+    // ensure that relative paths still resolve against the
+    // regular `node_modules` correctly
+    resolve('tap', {
+        paths: [
+            'node_path',
+        ],
+        basedir: 'node_path/x',
+    }, function (err, res) {
+        t.equal(res, path.resolve(__dirname, '..', 'node_modules/tap/lib/main.js'));
     });
 });
