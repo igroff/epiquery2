2a6f654c2d7db4edebe5315c58c56ae678e92d50
Phred Lane <plane@glgroup.com>
2020-10-15 19:23:44 -0500

    added missing tests

--- test/config.js
+++ test/config.js
@@ -43,6 +43,45 @@ describe('config', () => {
     });
   });
 
+  describe('with no connections in connection_config', () => {
+    beforeEach(() => {
+      envStub = sinon.stub(process, 'env').value({ CONNECTION_CONFIG: '{}' });
+    });
+
+    afterEach(() => {
+      envStub.restore();
+    });
+
+    it('should throw an error', () => {
+      assert.throws(() => {
+        proxyquire('../src/config.js', { simplog: log });
+      }, 'No connections specified');
+    });
+  });
+
+  describe('with ALLOWED_TEMPLATE_PATHS', () => {
+    const expected = { allowd: { template: { paths: +Date.now() } } };
+
+    let config;
+
+    beforeEach(() => {
+      envStub = sinon.stub(process, 'env').value({
+        ALLOWED_TEMPLATE_PATHS: JSON.stringify(expected),
+        CONNECTION_CONFIG: JSON.stringify({ meh: { name: 'meh' } }),
+      });
+
+      config = proxyquire('../src/config.js', { simplog: log });
+    });
+
+    afterEach(() => {
+      envStub.restore();
+    });
+
+    it('should parse ALLOWED_TEMPLATE_PATHS', () => {
+      assert.deepEqual(config.allowedTemplates, expected);
+    });
+  });
+
   describe('default values', () => {
     before(() => {
       envStub = sinon.stub(process, 'env').value({
@@ -76,7 +115,7 @@ describe('config', () => {
             name: 'bar',
             config: {},
           },
-          baz: { name: 'baz' },
+          bazinga: { name: 'bazinga' },
           catpants: {
             name: 'catpants',
             config: {
@@ -114,6 +153,42 @@ describe('config', () => {
             options: {
               password: 'catpants-password',
               userName: 'catpants-userName',
+            },
+          },
+        },
+      });
+    });
+
+    it('should copy user and password', () => {
+      assert.deepEqual(config.connections.foo, {
+        name: 'foo',
+        config: {
+          password: 'foo-password',
+          user: 'foo-user',
+          authentication: {
+            type: 'default',
+            options: {
+              password: 'foo-password',
+              userName: 'foo-user',
+            },
+          },
+        },
+      });
+    });
+
+    it('should not update connections without a config property', () => {
+      assert.deepEqual(config.connections.bazinga, { name: 'bazinga'});
+    });
+
+    it('should not error when username and password are not in config', () => {
+      assert.deepEqual(config.connections.bar, {
+        name: 'bar',
+        config: {
+          authentication: {
+            type: 'default',
+            options: {
+              password: undefined,
+              userName: undefined,
             },
           },
         },
