3cec1b2e88a0c25eb050d6f255d396723e3bc96b
Ryan Rauh <rauh.ryan@gmail.com>
2021-04-07 17:31:25 -0500

    Adds a postgres driver

---
+++ difftest/templates/test/postgres/select_6_rows.sql
@@ -0,0 +1,7 @@
+/*
+executionMasks:
+  a_valid_bitmask: 1
+*/
+
+SELECT * from generate_series(0, 5)
+
---
+++ src/drivers/postgresql.js
@@ -0,0 +1,73 @@
+const events = require('events');
+const log = require('simplog');
+const _ = require('lodash-contrib');
+const os = require('os');
+const pg = require('pg');
+const QueryStream = require('pg-query-stream');
+const JSONStream = require('JSONStream');
+
+class PostgresqlDriver  extends events.EventEmitter {
+  constructor(config) {
+    super();
+    this.config = config;
+    this.valid = false;
+  }
+
+  execute(query, context) {
+    log.debug(query, context.templateContext)
+    const stream = this.conn.query(new QueryStream(query));
+    stream.pipe(JSONStream.stringify())
+
+    let rowSetStarted = false;
+    stream.on('data', record => {
+      if (!rowSetStarted) {
+        rowSetStarted = true;
+        this.emit('beginrowset');
+      }
+      this.emit('row', record);
+    });
+    stream.on('end', query => {
+      if (rowSetStarted) {
+        this.emit('endrowset');
+      }
+      this.emit('endquery', query);
+    });
+    stream.on('error', error => {
+      if (rowSetStarted) {
+        this.emit('endrowset');
+      }
+      this.valid = false;
+      this.emit('error', error);
+    });
+  }
+
+  connect(cb) {
+    log.debug('connect', this.config);
+    this.conn = new pg.Client({
+      user: this.config.userName,
+      host: this.config.server,
+      password: this.config.password,
+      database: this.config.options.database,
+      port: this.config.options.port,
+    });
+    this.conn.connect(err => {
+      if(err) {
+        log.error(`Failed connecting to postgres \n${err}`)
+      } else {
+        this.valid = true;
+        log.debug("Connected to postgres");
+      }
+      cb(err, this);
+    });
+  }
+
+  disconnect() {
+    this.conn.end();
+  }
+
+  validate() {
+    return this.valid;
+  }
+}
+
+module.exports.DriverClass = PostgresqlDriver
