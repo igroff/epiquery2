06c56bae8f3ab8134d7bf01e614662f3a95b8251
Jim Clouse <jclouse@glgroup.com>
2023-03-23 00:28:48 -0700

    removing unused line

--- difftest/templates/test/param_json_is_json.sql
+++ difftest/templates/test/param_json_is_json.sql
@@ -5,8 +5,6 @@
 IF ISJSON(@myJson) > 0
     SELECT 'Valid Json' AS 'Result';
 
-declare @apples table (type varchar(32))
-
 SELECT type
 FROM OpenJson(@myJson, '$.apples') WITH (type VARCHAR(32) '$');
 
