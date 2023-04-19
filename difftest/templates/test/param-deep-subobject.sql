5118f9927d670b444b915c64244bbd2b978e7a03
Jim Clouse <jclouse@glgroup.com>
2023-04-19 10:34:56 -0700

    adding test for deep subobject happy path

---
+++ difftest/templates/test/param-deep-subobject.sql
@@ -0,0 +1,6 @@
+--@userId int user.userId
+--@userName varchar user.name
+--@petType varchar user.pet.type
+--@petName varchar user.pet.detail.name 
+
+select @userId as userId, @userName as name, @petType as petType, @petName as petName;
